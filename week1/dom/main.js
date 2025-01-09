601fc5d10ddf631cdd2548da6d9a953e6a379502
Cheng He <cheng.he@durhamcollege.ca>
2025-01-08 19:29:05 -0500

    dom

--- week1/dom/main.js
+++ week1/dom/main.js
@@ -1,61 +1,46 @@
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar').querySelector('ul');
 
-const btnDarken = document.querySelector('.dark');
-const btnAdd = document.querySelector('.add');
+const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
+/* Declaring the array of image filenames */
 const images = [
-  {file:'pic1.jpg', alt:'Human eye close up'}, 
-  {file:'pic2.jpg', alt:'Rock that resembles a wave'},
-  {file:'pic3.jpg', alt:'Purple and white flowers'},
-  {file:'pic4.jpg', alt:'Hieroglyphics of pharaoh'},
-  {file:'pic5.jpg', alt:'Moth on a leaf'}]
+  { file: 'pic1.jpg', alt: 'Human eye close up' },
+  { file: 'pic2.jpg', alt: 'Rock that resembles a wave' },
+  { file: 'pic3.jpg', alt: 'Purple and white flowers' },
+  { file: 'pic4.jpg', alt: 'Hieroglyphics of pharaoh' },
+  { file: 'pic5.jpg', alt: 'Moth on a leaf' }]
 
 /*demo static NodeList vs live HTMLCollection*/
 var itemNode = thumbBar.querySelectorAll('.image');
-var itemElement = thumbBar.getElementsByClassName('image')
-btnAdd.addEventListener('click', (evt) => {
-  for (const image of images) {
-    const newItem = document.createElement('li');
-    newItem.setAttribute('class','image');
-    const newImage = document.createElement('input');
-    newImage.setAttribute('type', 'image');
-    newImage.setAttribute('src', `images/${image.file}`);
-    newImage.setAttribute('alt', `images/${image.alt}`);
-    newItem.appendChild(newImage);
-    thumbBar.appendChild(newItem);
-    newImage.addEventListener('click', e => {
-        displayedImage.src = e.target.src;
-        displayedImage.alt =e.target.alt;
-    }); 
-  }
+var itemElement = thumbBar.getElementsByClassName('image');
+for (const image of images) {
+  const newItem = document.createElement('li');
+  newItem.setAttribute('class','image');
+  const newImage = document.createElement('input');
+  newImage.setAttribute('type', 'image');
+  newImage.setAttribute('src', `images/${image.file}`);
+  newImage.setAttribute('alt', `images/${image.alt}`);
+  newItem.appendChild(newImage);
+  thumbBar.appendChild(newItem);
+  newImage.addEventListener('click', e => {
+    displayedImage.src = e.target.src;
+    displayedImage.alt = e.target.alt;
+  });
+}
 /*Selector Type: querySelectorAll uses CSS selectors, while getElementsByClassName uses class names.
 Return Type: querySelectorAll returns a static NodeList, whereas getElementsByClassName returns a live HTMLCollection.
 Flexibility: querySelectorAll is more flexible as it can use any CSS selector, not just class names
-*/  
-  console.log(itemNode);
-  itemNode = thumbBar.querySelectorAll('.image');
-  console.log(itemElement);
-}); 
+*/
+console.log(itemNode);
+itemNode = thumbBar.querySelectorAll('.image');
+console.log(itemElement);
 
-/* Wiring up the Darken/Lighten button */
-btnDarken.addEventListener('click', (evt) => {
-    const btnClass = evt.currentTarget.getAttribute('class');
-    if (btnClass === 'dark') {
-      evt.currentTarget.setAttribute('class','light');
-      evt.currentTarget.textContent = 'Lighten';
-      overlay.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-    } else {
-      evt.currentTarget.setAttribute('class','dark');
-      evt.currentTarget.textContent = 'Darken';
-      overlay.style.backgroundColor = 'rgb(0 0 0 / 0%)';
-    }
-  });
 //EventTarget <-- Node <-- Document  
 //https://developer.mozilla.org/en-US/docs/Web/API/Node
 console.log(document.childNodes); //https://dmitripavlutin.com/dom-node-element/
 //https://developer.mozilla.org/en-US/docs/Web/API/Document
 console.log(document.children);
 console.log(document.children[0].childNodes);
-console.log(document.children[0].children);
+console.log(document.children[0].children);  
--- week2/event/main.js
+++ week2/event/main.js
@@ -10,35 +10,48 @@ function setup(){
   const displayedImage = document.querySelector('.displayed-img');
   const thumbBar = document.querySelector('.thumb-bar').querySelector('ul');
   
-  const btn = document.querySelector('button');
+  const btnDarken = document.querySelector('.dark');
+  const btnAdd = document.querySelector('.add');
   const overlay = document.querySelector('.overlay');
 
-  for (const image of images) {
-    const newItem = document.createElement('li');
-    const newImage = document.createElement('input');
-    newImage.setAttribute('type', 'image');
-    newImage.setAttribute('src', `images/${image.file}`);
-    newImage.setAttribute('alt', `images/${image.alt}`);
-    newItem.appendChild(newImage);
-    thumbBar.appendChild(newItem);
-    newImage.addEventListener('click', e => {
-        displayedImage.src = e.target.src;
-        displayedImage.alt =e.target.alt;
-    }); 
-  }
-
-  btn.addEventListener('click', () => {
-    const btnClass = btn.getAttribute('class');
+ /*demo static NodeList vs live HTMLCollection*/
+  var itemNode = thumbBar.querySelectorAll('.image');
+  var itemElement = thumbBar.getElementsByClassName('image') 
+  btnDarken.addEventListener('click', (evt) => {
+    const btnClass = evt.currentTarget.getAttribute('class');
     if (btnClass === 'dark') {
-      btn.setAttribute('class','light');
-      btn.textContent = 'Lighten';
+      evt.currentTarget.setAttribute('class','light');
+      evt.currentTarget.textContent = 'Lighten';
       overlay.style.backgroundColor = 'rgb(0 0 0 / 50%)';
     } else {
-      btn.setAttribute('class','dark');
-      btn.textContent = 'Darken';
+      evt.currentTarget.setAttribute('class','dark');
+      evt.currentTarget.textContent = 'Darken';
       overlay.style.backgroundColor = 'rgb(0 0 0 / 0%)';
     }
   });
+  btnAdd.addEventListener('click', (evt) => {
+    for (const image of images) {
+      const newItem = document.createElement('li');
+      newItem.setAttribute('class','image');
+      const newImage = document.createElement('input');
+      newImage.setAttribute('type', 'image');
+      newImage.setAttribute('src', `images/${image.file}`);
+      newImage.setAttribute('alt', `images/${image.alt}`);
+      newItem.appendChild(newImage);
+      thumbBar.appendChild(newItem);
+      newImage.addEventListener('click', e => {
+          displayedImage.src = e.target.src;
+          displayedImage.alt =e.target.alt;
+      }); 
+    }
+  /*Selector Type: querySelectorAll uses CSS selectors, while getElementsByClassName uses class names.
+  Return Type: querySelectorAll returns a static NodeList, whereas getElementsByClassName returns a live HTMLCollection.
+  Flexibility: querySelectorAll is more flexible as it can use any CSS selector, not just class names
+  */  
+    console.log(itemNode);
+    itemNode = thumbBar.querySelectorAll('.image');
+    console.log(itemElement);
+  });   
 }
 
 function bodyLoad(event){
@@ -49,3 +62,11 @@ function bodyLoad(event){
 document.addEventListener('DOMContentLoaded', (evt)=>{
   console.log('DOMContentLoaded');
 });
+
+//EventTarget <-- Node <-- Document  
+//https://developer.mozilla.org/en-US/docs/Web/API/Node
+console.log(document.childNodes); //https://dmitripavlutin.com/dom-node-element/
+//https://developer.mozilla.org/en-US/docs/Web/API/Document
+console.log(document.children);
+console.log(document.children[0].childNodes);
+console.log(document.children[0].children);
