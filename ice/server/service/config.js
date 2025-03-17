e1b5ab6c62ec1e85a9ad333c7a085595ce7f59cf
Cheng He <cheng.he@durhamcollege.ca>
2025-03-17 08:36:48 -0400

    logging

--- ice/server/service/config.js
+++ ice/server/service/config.js
@@ -1,11 +1,14 @@
 import bodyParser from 'body-parser';
 import animalRouter from '../routes/animal.js'
+import { loggingMiddleware } from '../middleWare/logging.js';
 
 function config(app) {
     
     // Parse JSON bodies and URL-encoded bodies
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
+    
+    app.use(loggingMiddleware);
 
     let helloContent = `<!DOCTYPE html><html lang=\"en-us\"><head><title>INFT 2202</title></head><body><main><h1>Hello from Express</h1><p>at ${new Date()}</p></main></body></html>`;
     app.get("/hello", (req, res) => {
